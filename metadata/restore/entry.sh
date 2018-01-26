37260e2e203d485c119d94a5d312a8132136f864
Vadim Zharov <urswamp@gmail.com>
2018-01-26 20:19:17 +0300

    working commit

--- metadata/restore/entry.sh
+++ metadata/restore/entry.sh
@@ -34,69 +34,33 @@ mkdir -p $TMP_DIR
 
 echo "Working using token $TOKEN"
 
-echo "Starting project backup...."
+echo "Starting project restore...."
 
 echo "=============================================================="
 
-echo "Backuping Project description..."
-
-curl --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
-    -H "Authorization: Bearer $TOKEN" \
-    -H 'Accept: application/yaml' \
-    https://openshift.default.svc.cluster.local/oapi/v1/projects/$PROJECT_NAME > $TMP_DIR/$PROJECT_NAME-project.yaml
-
-while read api; do
-    echo "Backuping $api on https://openshift.default.svc.cluster.local/oapi/v1/namespaces/$PROJECT_NAME/$api"
-    curl --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
-    -H "Authorization: Bearer $TOKEN" \
-    -H 'Accept: application/yaml' \
-    https://openshift.default.svc.cluster.local/oapi/v1/namespaces/$PROJECT_NAME/$api > $TMP_DIR/$PROJECT_NAME-$api.yaml
-done < /restic-openshift-oapi.cfg
-
-while read api; do
-    echo "Backuping $api on https://openshift.default.svc.cluster.local/api/v1/namespaces/$PROJECT_NAME/$api"
-    curl --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
-    -H "Authorization: Bearer $TOKEN" \
-    -H 'Accept: application/yaml' \
-    https://openshift.default.svc.cluster.local/api/v1/namespaces/$PROJECT_NAME/$api > $TMP_DIR/$PROJECT_NAME-$api.yaml
-    echo "=============================================================="
-done < /restic-openshift-api.cfg
-
-echo "=============================================================="
-
-restic -r $RESTIC_REPOSITORY backup $TMP_DIR/*.yaml --tag metadata --tag $PROJECT_NAME --tag $RESTIC_TAG --hostname $PROJECT_NAME --cache-dir /tmp/ 2>&1
-
-rc=$?
-
-if [[ $rc == 0 ]]; then
-    echo "Backup Successfull" 
-else
-    echo "Backup Failed with Status ${rc}"
-    restic unlock
-    exit
-fi
-
-
-echo "==================================="
-echo "Starting prune process..."
-echo "-----------------------------------"
-
-restic -r $RESTIC_REPOSITORY forget --keep-last $RESTIC_KEEP --prune --tag metadata --tag $PROJECT_NAME --tag $RESTIC_TAG --cache-dir /tmp/
-
-rc=$?
-
-if [[ $rc == 0 ]]; then
-    echo "Prune Successfull" 
-else
-    echo "Prune Failed with Status ${rc}"
-    restic unlock
-    exit
-fi
-
-
-echo "-----------------------------------"
-echo "Pruning complete!"
-echo "==================================="
-echo "Current backups in repositories:"
+echo "Current snapshots in repository:"
 
 restic -r $RESTIC_REPOSITORY snapshots --cache-dir /tmp/
+
+echo "=============================================================="
+
+#restic -r $RESTIC_REPOSITORY backup $TMP_DIR/*.yaml --tag metadata --tag $PROJECT_NAME --tag $RESTIC_TAG --hostname $PROJECT_NAME --cache-dir /tmp/ 2>&1
+
+echo "Restoring files using snapshot ${RESTIC_SNAPSHOT}"
+
+restic -r $RESTIC_REPOSITORY  restore $RESTIC_SNAPSHOT --target $TMP_DIR
+
+rc=$?
+
+if [[ $rc == 0 ]]; then
+    echo "Restore successfull" 
+else
+    echo "Restore Failed with Status ${rc}"
+    restic unlock
+    exit
+fi
+
+
+
+
+
