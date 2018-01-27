3cfd58b7f20be3afba4f04cc2708024a6949d643
Vadim Zharov <urswamp@gmail.com>
2018-01-27 20:57:44 +0300

    working commit

---
+++ all-files/backup/entry.sh
@@ -0,0 +1,31 @@
+#!bin/sh
+set +e
+
+echo "Starting container ..."
+echo "====================================="
+echo "Parameters:"
+echo "Source: project metadata $PROJECT_NAME"
+echo "Destination: $RESTIC_DESTINATION"
+echo "Repository password: "$RESTIC_PASSWORD
+echo "Backup tag: " $RESTIC_TAG
+echo "Project name: " $PROJECT_NAME
+echo "===================================="
+
+TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+
+echo "Starting backup jobs to backup all PVCs in current project"
+
+echo "=============================================================="
+
+echo "Will try to backup these PVCs:"
+
+oc get pvc --no-headers=true | awk '{print $1}'
+
+while read -r pvc ; do
+    echo "Processing $pvc"
+    # your code goes here
+done < oc get pvc --no-headers=true | awk '{print $1}'
+
+
+
+
