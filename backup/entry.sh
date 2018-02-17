91dafe7ec509ed74948e46c8d51d19cd15efc48c
Vadim Zharov <urswamp@gmail.com>
2018-02-18 01:32:37 +0300

    working commit

--- backup/entry.sh
+++ backup/entry.sh
@@ -5,7 +5,7 @@ echo "======================================================================"
 echo " Parameters:                                                          "
 echo " Will backup: $BACKUP_TYPE for project $PROJECT_NAME                  "
 echo " Destination: $RESTIC_DESTINATION                                     "
-echo " Host $RESTIC_HOST:$RESTIC_PORT                                       "
+echo " Host $RESTIC_S3_HOST:$RESTIC_S3_PORT                                       "
 echo " Repository password: $RESTIC_PASSWORD                                "
 echo " Backup tag: $RESTIC_TAG                                              "
 echo " Will keep $RESTIC_KEEP copies of data                                "
@@ -26,9 +26,11 @@ case $BACKUP_TYPE in
     metadata)
         echo "Will try to backup project metadata"
         metadata-backup.sh
+        ;;
     files)
         echo "Will try to backup files from one PVC"
         files-backup.sh
+        ;;
 esac
 
 rc=$?
