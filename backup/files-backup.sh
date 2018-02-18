ab47c8c6f97a321eb2151ef92d2e00d438cc7e21
Vadim Zharov <urswamp@gmail.com>
2018-02-18 23:14:40 +0300

    working commit

--- backup/files-backup.sh
+++ backup/files-backup.sh
@@ -5,10 +5,6 @@ echo "+=====================================+"
 echo "| Starting script to make FILE backup |"
 echo "+=====================================+"
 
-echo "Total size for backup:"
-
-df -h /data | awk {'print $3'} | sed '2!D'
-
 echo "Starting backup...."
 
 restic backup /data --tag files --tag $PROJECT_NAME --tag $RESTIC_TAG --hostname $PROJECT_NAME --exclude=$RESTIC_EXCLUDE --cache-dir /tmp/ 2>&1
