07edcf7ae0483425ce04dfb98cf7ad2ca6f80f2d
Vadim Zharov <urswamp@gmail.com>
2018-02-20 21:30:42 +0300

    working commit

--- backup/databases-backup.sh
+++ backup/databases-backup.sh
@@ -3,26 +3,27 @@ set +e
 
 echo "+====================================================+"
 echo "| Starting script to make DATABASE backup            |"
-echo "| Will look for config in /pg_backup & /mysql_backup |"
 echo "+====================================================+"
 
-while read DBPARAM; do
-    DATABASE_SVC=$(echo $DBPARAM | awk '{print $1}')
-    DATABASE_USER=$(echo $DBPARAM | awk '{print $2}')
-    PGPASSWORD=$(echo $DBPARAM | awk '{print $3}')
-    echo "Perform backup for database with address $DATABASE_SVC"
-    pg_dumpall -h $DATABASE_SVC -U $DATABASE_USER | restic backup -r $RESTIC_REPOSITORY --tag databases --tag $PROJECT_NAME --tag $DATABASE_SVC --hostname $PROJECT_NAME --stdin --stdin-filename $DATABASE_SVC.sql --cache-dir /tmp/
-done < /pg_backup
+case $DATABASE_TYPE in
+    postgresql)
+        echo "Will try to backup postgresql on service $DATABASE_SVC with user $DATABASE_USER and password $DATABASE_PASSWORD"
+        export PGPQSSWORD=$DATABASE_PASSWORD
+        pg_dumpall -h $DATABASE_SVC -U $DATABASE_USER | restic backup -r $RESTIC_REPOSITORY --tag databases --tag $PROJECT_NAME --tag $DATABASE_SVC --hostname $PROJECT_NAME --stdin --stdin-filename $DATABASE_SVC.sql --cache-dir /tmp/    
+        ;;
+    mysql)
+        echo "Will try to backup mysql on service $DATABASE_SVC with user $DATABASE_USER and password $DATABASE_PASSWORD"
+        mysqldump -h $DATABASE_SVC -u $DATABASE_USER --password=$DATABASE_PASSWORD -C --all-databases | restic backup -r $RESTIC_REPOSITORY --stdin --tag databases --tag $PROJECT_NAME --tag $DATABASE_SVC --hostname $PROJECT_NAME --stdin-filename $DATABASE_SVC.sql --cache-dir /tmp/    
+        ;;
+esac
 
-while read DBPARAM; do
-    DATABASE_SVC=$(echo $DBPARAM | awk '{print $1}')
-    DATABASE_USER=$(echo $DBPARAM | awk '{print $2}')
-    DATABASE_PASSWORD=$(echo $DBPARAM | awk '{print $3}')
-    echo "Perform backup for database with address $DATABASE_SVC"
-    mysqldump -h $DATABASE_SVC -u $DATABASE_USER --password=$DATABASE_PASSWORD -C --all-databases | restic backup -r $RESTIC_REPOSITORY --stdin --tag databases --tag $PROJECT_NAME --tag $DATABASE_SVC --hostname $PROJECT_NAME --stdin-filename $DATABASE_SVC.sql --cache-dir /tmp/
-done < /mysql_backup
+rc=$?
 
-restic backup /mysql_backup --tag databases --tag $PROJECT_NAME --tag $RESTIC_TAG --hostname $PROJECT_NAME --cache-dir /tmp/ 2>&1
+if [[ $rc == 0 ]]; then
+    echo "dbdump and backup process was successful" 
+else
+    echo "Backup failed with status ${rc}"
+    exit
+fi
 
-restic backup /pg_backup --tag databases --tag $PROJECT_NAME --tag $RESTIC_TAG --hostname $PROJECT_NAME --cache-dir /tmp/ 2>&1
 
