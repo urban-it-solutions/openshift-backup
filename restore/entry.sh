1094c50900ff1a495130c9aea101e07712a8afbf
Vadim Zharov <urswamp@gmail.com>
2018-02-27 00:26:08 +0300

    Trying to remove restore-all-pvc

--- restore/entry.sh
+++ restore/entry.sh
@@ -10,39 +10,45 @@ echo " Repository password: $RESTIC_PASSWORD                                "
 echo " Backup tag: $RESTIC_TAG                                              "
 echo "======================================================================"
 
-if [[ $BACKUP_TYPE='all-pvc' ]]; then
-    export RESTORE_TYPE='metadata'
-    export RESTIC_TAG='metadata'
-else
-    export RESTORE_TYPE=$BACKUP_TYPE
-fi
+#if [ $BACKUP_TYPE='all-pvc' ]; then
+#    export RESTORE_TYPE='metadata'
+#    export RESTIC_TAG='metadata'
+#else
+#    export RESTORE_TYPE=$BACKUP_TYPE
+#fi
 
 case $RESTIC_DESTINATION in
     s3)
         echo "Will restore from S3 generic (like Minio) object store - $RESTIC_HOST:$RESTIC_S3_PORT"
-        export RESTIC_REPOSITORY=s3:http://$RESTIC_HOST:$RESTIC_S3_PORT/$PROJECT_NAME/$RESTORE_TYPE/$RESTIC_TAG
+#        export RESTIC_REPOSITORY=s3:http://$RESTIC_HOST:$RESTIC_S3_PORT/$PROJECT_NAME/$RESTORE_TYPE/$RESTIC_TAG
+        export RESTIC_REPOSITORY=s3:http://$RESTIC_HOST:$RESTIC_S3_PORT/$PROJECT_NAME
     ;;
     aws)
         echo "Will restore from AMAZON S3 storage - $RESTIC_HOST"
-        export RESTIC_REPOSITORY=s3:$RESTIC_HOST/$PROJECT_NAME/$RESTORE_TYPE/$RESTIC_TAG
+#        export RESTIC_REPOSITORY=s3:$RESTIC_HOST/$PROJECT_NAME/$RESTORE_TYPE/$RESTIC_TAG
+        export RESTIC_REPOSITORY=s3:$RESTIC_HOST/$PROJECT_NAME
     ;;
 esac
 
 case $BACKUP_TYPE in
     metadata)
         echo "Will try to restore project metadata"
+        export RESTIC_REPOSITORY=$RESTIC_REPOSITORY/$BACKUP_TYPE/$RESTIC_TAG
         ./metadata-restore.sh
         ;;
     files)
         echo "Will try to restore files for PVC $RESTIC_TAG"
+        export RESTIC_REPOSITORY=$RESTIC_REPOSITORY/$BACKUP_TYPE/$RESTIC_TAG
         ./files-restore.sh
         ;;
     databases)
         echo "Will try to restore database with service $DATABASE_SVC"
+        export RESTIC_REPOSITORY=$RESTIC_REPOSITORY/$BACKUP_TYPE/$RESTIC_TAG
         ./databases-restore.sh
         ;;
     all-pvc)
         echo "Will try to restore files for all PVC's"
+        export RESTIC_REPOSITORY=$RESTIC_REPOSITORY/metadata/metadata
         ./restore-all-pvc.sh
         ;;
 esac
