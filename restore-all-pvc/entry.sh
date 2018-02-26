e87b812c46cb4d49c7964fbe5b6328f95a76b047
Vadim Zharov <urswamp@gmail.com>
2018-02-26 20:57:43 +0300

    added DisplayNames into templates & fixed template params in all-files-restore

--- restore-all-pvc/entry.sh
+++ restore-all-pvc/entry.sh
@@ -32,5 +32,5 @@ done
 
 oc get pvc --no-headers=true | awk '{print $1}' | while read -r pvc ; do
     echo "Processing $pvc..."
-    oc process restore-project-files -p=CUSTOM_TAG=$pvc -p=JOB_NAME=restore-files-for-pvc-$pvc -p=RESTIC_DESTINATION=$RESTIC_DESTINATION -p=RESTIC_HOST=$RESTIC_HOST -p=RESTIC_S3_PORT=$RESTIC_S3_PORT -p=RESTIC_EXCLUDE=$RESTIC_EXCLUDE -p=RESTIC_KEEP=$RESTIC_KEEP -p=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -p=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -p RESTIC_SNAPSHOT=$RESTIC_SNAPSHOT | oc create -f -
+    oc process restore-project-files -p=CUSTOM_TAG=$pvc -p=JOB_NAME=restore-files-for-pvc-$pvc -p=RESTIC_DESTINATION=$RESTIC_DESTINATION -p=RESTIC_HOST=$RESTIC_HOST -p=RESTIC_S3_PORT=$RESTIC_S3_PORT -p=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -p=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -p RESTIC_SNAPSHOT=$RESTIC_SNAPSHOT | oc create -f -
 done
