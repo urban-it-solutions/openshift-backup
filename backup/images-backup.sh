a242fdac816717e89141a9dec73c32ced4c95f35
Vadim <urswamp@gmail.com>
2018-03-16 22:57:44 +0300

    fixed skopeo

--- backup/images-backup.sh
+++ backup/images-backup.sh
@@ -17,8 +17,9 @@ echo "Get images from image streams into temp dir"
     
 oc get is -o custom-columns='NAME:.metadata.name' --no-headers=true | while read -r is_name ; do
     echo "Processing $is_name..."
-    is_path=$(oc get is $is_name --no-headers | awk '{print $2":" $3}')
-    skopeo copy --src-tls-verify=false --src-creds backup-sa:$TOKEN docker://$is_path docker-archive:$TMP_DIR/$is_name.tar.gz
+    is_path=$(oc get is $is_name --no-headers | awk '{print $2}')
+    is_tag=$(oc get is $is_name --no-headers | awk '{print $3}')
+    skopeo copy --src-tls-verify=false --src-creds backup-sa:$TOKEN docker://$is_path docker-archive:$TMP_DIR/$is_name:is_tag
 done
 
 restic -r $RESTIC_REPOSITORY backup $TMP_DIR/*.tar.gz --tag images --tag $PROJECT_NAME --tag $RESTIC_TAG --hostname $PROJECT_NAME --cache-dir /tmp/ 2>&1
